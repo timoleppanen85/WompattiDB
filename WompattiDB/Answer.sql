152d171873dc933cde9034844eb2d077fbf823a7
timoleppanen85 <31985525+timoleppanen85@users.noreply.github.com>
2019-11-03 14:27:24 +0200

    Added timestamp to Answer
    Default value GETUTCNOW()

--- WompattiDB/Answer.sql
+++ WompattiDB/Answer.sql
@@ -6,6 +6,7 @@
     [AnswerSet] INT NOT NULL, 
     [QuestionId] BIGINT NOT NULL, 
     [UserId] BIGINT NOT NULL, 
+    [Timestamp] DATE NOT NULL DEFAULT GETUTCDATE(), 
     CONSTRAINT [FK_Answer_Question] FOREIGN KEY ([QuestionId]) REFERENCES [Question]([Id]), 
     CONSTRAINT [FK_Answer_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
 	ON DELETE CASCADE
