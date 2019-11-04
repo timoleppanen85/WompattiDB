c314223507678d2ffb536a88bb677e41ff55b728
Timo Leppänen <timo.leppanen85@gmail.com>
2019-11-04 12:04:48 +0200

    Removed personal info

--- WompattiDB/User.sql
+++ WompattiDB/User.sql
@@ -1,8 +1,5 @@
 ﻿CREATE TABLE [dbo].[User]
 (
 	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
-    [Username] NVARCHAR(50) NOT NULL, 
-    [Firstname] NVARCHAR(50) NULL, 
-    [Lastname] NVARCHAR(50) NULL, 
-    [Photo] NVARCHAR(MAX) NULL
+    [Username] NVARCHAR(50) NOT NULL 
 )
